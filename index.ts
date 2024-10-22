73ea19da0631a14170854a06b6f5e7d9d558471d
OsamaKhaled9 <osamakhaled236@gmail.com>
2024-10-22 16:27:33 +0300

    Final

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import { UserRepository } from './UserRepository';
-
 (async () => {
-    let id = 4;
-    let username = 'Pablo Escobar';
+    let id = 5;
+    let username = 'Yahya_Gad';
     const repository = new UserRepository();
 
-    await repository.addUser({ id, name: username, email: `${username}@example.com` });
+    // Await the result of getUserById to check if the user already exists
+    const existingUser = await repository.getUserById(id);
+
+    if (existingUser == null) { // If no user with the given ID exists
+        await repository.addUser({ id, name: username, email: `${username}@example.com` });
+        console.log(`User ${username} added.`);
+    } else {
+        console.log(`User ${username} already exists`);
+    }
 
     // Retrieve all users
     const users = await repository.getAllUsers();
